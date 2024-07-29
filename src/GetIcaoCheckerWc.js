4c9a4bd45c3f7ce0e053440997a23e463296700c
Ali Helmi <ahelmi365@gmail.com>
2024-07-29 11:14:25 +0300

    update version number

--- src/GetIcaoCheckerWc.js
+++ src/GetIcaoCheckerWc.js
@@ -83,7 +83,7 @@ export class GetIcaoCheckerWc extends LitElement {
   constructor() {
     super();
 
-    console.log("constructor version 1.3.4");
+    console.log("constructor version 1.3.5");
 
     this.isICAOWC = false;
     this.openModalElmId = "open-icao-modal";
